1aa053146f248c18bc854c94f74c1ab4b4532316
Chris Pickett <chris.pickett@gmail.com>
2017-07-10 16:50:00 -0700

    Update WProgram.h -> Arduino.h to support newer IDEs

--- Candle.h
+++ Candle.h
@@ -1,19 +1,19 @@
 #ifndef Candle_h
 #define Candle_h
 
-#include "WProgram.h"
+#include "Arduino.h"
 
 class Candle
 {
   public:
     void setup(int pin);
     void flicker();
-    
+
   private:
     int _pin;
     int _brightness;
     int _flickerShift;
-    
+
     unsigned long _nextFlicker;
 };
 
